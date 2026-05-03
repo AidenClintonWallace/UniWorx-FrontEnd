25e5c828006869e77a8e00d3e665629caa869e52
Aiden wallace <222471174@mycput.ac.za>
2026-05-03 11:58:19 +0200

    initial commit

---
+++ src/pages/App.tsx
@@ -0,0 +1,11 @@
+import '../index.css';
+
+export default function App(){
+  return (
+    <div className="justify-center items-center border-2">
+      <h1 className='text-center'>
+        Home
+      </h1>
+    </div>
+  )
+}
